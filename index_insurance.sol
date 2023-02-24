aecae44c68f16d34d2b59be59e8045f634a9fddf
mxlazi <117105103+mxlazi@users.noreply.github.com>
2023-02-24 12:14:17 +0100

    Add files via upload

--- index_insurance.sol
+++ index_insurance.sol
@@ -11,9 +11,18 @@ contract DroughtInsurance {
   uint public premium;
   uint public payout;
   uint public indexThreshold;
-  address public farmer;
   address public oracle;
   address public creator;
+
+  struct Farmer {
+    string firstName;
+    string lastName;
+    uint region;
+    address farmerAccount;
+    }
+
+  Farmer[] public farmers;
+  mapping (uint => address) public obligationToFarmer;
 
   // Initializing the contract and setting the variables
   constructor(uint _premium, uint _payout, uint _indexThreshold, address _oracleAddress) {
@@ -22,6 +31,11 @@ contract DroughtInsurance {
     indexThreshold = _indexThreshold;
     oracle = _oracleAddress;
     creator = msg.sender;
+  }
+
+  // farmer registration
+  function _register(string _firstName, string _lastName, uint _region) internal {
+    farmers.push(Farmer(_firstName, _lastName, _region, msg.sender));
   }
 
   // Creating the function to check if a drought has occurred
@@ -39,30 +53,30 @@ contract DroughtInsurance {
   // Creating the function to buy the insurance (only purchasable the year before)
   function buyInsurance() public payable {
     require(msg.value == premium, "Incorrect premium amount.");
-    //require(block.timestamp);
-
+    obligationToFarmer[msg.sender] = msg.value;
   }
 
   // Setting Premium in case of risk increase/decrease (only SC owner should be able to do it)
   function setPremium(uint newPremium) public {
+    require(msg.sender == creator, "Only smart contract owner can set premium.");
     premium = newPremium;
   }
   
   // Setting Payout in case of risk increase/decrease (only SC owner should be able to do it)
   function setPayout(uint newPayout) public {
+    require(msg.sender == creator, "Only smart contract owner can set payout.");
     payout = newPayout;
   }
 
   // Claim
   function claim(uint amount) public {
-    require(msg.sender == farmer, "Only the farmer can claim the insurance.");
-
     // Checking if a drought has occurred
     bool isDrought = checkDrought();
 
     // Paying out the insurance if a drought has occurred
     if (isDrought) {
-      payable(msg.sender).transfer(payout);
+      payable(msg.sender).transfer(amount); // send claimed amount
+      obligationToFarmer[msg.sender] -= amount; // reduce obligation by amount claimed
     }
   }
 }
